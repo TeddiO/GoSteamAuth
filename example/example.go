46438ac049d89f9d11c79973cd7d7d7732976359
Teddi <teddi.orange@gmail.com>
2020-12-07 15:45:08 +0000

    Library Implementation

---
+++ example/example.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+
+	gosteamauth "../src"
+)
+
+func main() {
+	serverRouter := http.NewServeMux()
+	server := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		Handler:      serverRouter,
+	}
+
+	serverRouter.HandleFunc("/", ExamplePage)
+	serverRouter.HandleFunc("/process", ProcessSteamLogin)
+	log.Fatal(server.ListenAndServe())
+}
+
+func ExamplePage(resp http.ResponseWriter, req *http.Request) {
+	queryString := req.URL.Query()
+
+	if queryString.Get("login") == "true" {
+		gosteamauth.RedirectClient(resp, req, gosteamauth.ConstructURL("http://localhost:8080/process"))
+		return
+	}
+
+	loadingTemplate := template.New("index.html")
+	loadingTemplate, _ = template.ParseFiles("index.html")
+
+	if err := loadingTemplate.Execute(resp, nil); err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func ProcessSteamLogin(resp http.ResponseWriter, req *http.Request) {
+	queryString, _ := url.ParseQuery(req.URL.RawQuery)
+
+	// Lack of generics means we're joining to transform the queryString (Type: Values) in to a Map.
+	queryMap := gosteamauth.ValuesToMap(queryString)
+
+	steamID64, isValid, err := gosteamauth.ValidateResponse(queryMap)
+	if err != nil {
+		fmt.Fprintf(resp, "Failed to log in\nError: %s", err)
+		return
+	}
+
+	if isValid {
+		fmt.Fprintf(resp, "Successfully logged in!\nSteamID: %s", steamID64)
+	} else {
+		io.WriteString(resp, "Failed to log in.")
+	}
+
+}
--- src/gosteam.go
+++ src/gosteam.go
@@ -2,18 +2,32 @@ package gosteamauth
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"net/url"
+	"regexp"
 	"strings"
+	"time"
 )
+
+var validCredRx *regexp.Regexp
+var steamRx *regexp.Regexp
+var provider string = "https://steamcommunity.com/openid/login"
+
+func init() {
+	validCredRx = regexp.MustCompile("is_valid:true")
+	steamRx = regexp.MustCompile(`https://steamcommunity\.com/openid/id/(\d+)`)
+}
 
 func StringMapToString(theMap map[string]string) string {
 
 	mapLength := len(theMap)
-	strSeparator := ","
+	strSeparator := "&"
 	i := 1
 
 	var builder strings.Builder
+	builder.Grow(66) // We already roughly know our base size.
 
 	for k, v := range theMap {
 
@@ -23,7 +37,7 @@ func StringMapToString(theMap map[string]string) string {
 
 		i++
 
-		fmt.Fprintf(&builder, "%s=%s%s", k, v, strSeparator)
+		fmt.Fprintf(&builder, "%s=%s%s", k, url.QueryEscape(v), strSeparator)
 	}
 
 	return builder.String()
@@ -35,21 +49,81 @@ func ConstructURL(responsePath string) string {
 		log.Fatal("http was not found in the responsePath!")
 	}
 
-	// Omit the other parameters we used to put here.
-	// Those links are long since dead.
-	openIdParameters := map[string]string{
-		"openid.mode":      "checkid_setup",
-		"openid.return_to": responsePath,
-		"openid.realm":     responsePath,
+	if responsePath[5:5] != "s" {
+		log.Println("https isn't being used! Is this intentional?")
 	}
 
-	return url.QueryEscape(StringMapToString(openIdParameters))
+	// Even though the below URLs no longer function, the oauth 2.0 process formally calls
+	// for them and Valve actively checks for their presence.
+	openIdParameters := map[string]string{
+		"openid.mode":       "checkid_setup",
+		"openid.return_to":  responsePath,
+		"openid.realm":      responsePath,
+		"openid.ns":         "http://specs.openid.net/auth/2.0",
+		"openid.identity":   "http://specs.openid.net/auth/2.0/identifier_select",
+		"openid.claimed_id": "http://specs.openid.net/auth/2.0/identifier_select",
+	}
+
+	return StringMapToString(openIdParameters)
 }
 
-func ValidateResponse(something map[string]string) {
+func ValidateResponse(results map[string]string) (steamID64 string, isValid bool, err error) {
 
+	openIdValidation := map[string]string{
+		"openid.assoc_handle": results["openid.assoc_handle"],
+		"openid.signed":       results["openid.signed"],
+		"openid.sig":          results["openid.sig"],
+		"openid.ns":           results["openid.ns"],
+		"openid.mode":         "check_authentication",
+	}
+
+	signedParams := strings.Split(results["openid.signed"], ",")
+
+	for _, value := range signedParams {
+		item := fmt.Sprintf("openid.%s", value)
+		if _, exists := openIdValidation[item]; !exists {
+			openIdValidation[item] = results[item]
+		}
+	}
+
+	urlObj, _ := url.Parse(provider)
+	urlObj.RawQuery = StringMapToString(openIdValidation)
+
+	httpClient := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	validationResp, err := httpClient.Get(urlObj.String())
+	if err != nil {
+		log.Printf("Failed to validate %s. Error: %s ", results["openid.claimed_id"], err)
+		return "", false, err
+	}
+
+	defer validationResp.Body.Close()
+	returnedBytes, err := ioutil.ReadAll(validationResp.Body)
+	if err != nil {
+		return "", false, err
+	}
+
+	if validCredRx.MatchString(string(returnedBytes)) == true {
+		return steamRx.FindStringSubmatch(results["openid.claimed_id"])[1], true, nil
+	}
+
+	return "", false, nil
 }
 
-func RedirectClient() {
+func RedirectClient(response http.ResponseWriter, request *http.Request, returnPath string) {
+	response.Header().Add("Content-Type", "application/x-www-form-urlencoded")
+	http.Redirect(response, request, fmt.Sprintf("%s?%s", provider, returnPath), 303)
+	return
+}
 
+// Utility function
+func ValuesToMap(fakeMap url.Values) map[string]string {
+	returnMap := map[string]string{}
+	for k, v := range fakeMap {
+		returnMap[k] = v[0]
+	}
+
+	return returnMap
 }
